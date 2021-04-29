aabb7a726f34b56e7674c67e5513b142da1e662b
Emival Santos <emivalvictor@hotmail.com>
2021-04-29 14:38:08 -0300

    Chore: Initial setup

---
+++ styles/globalstyles.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+import { Reset } from "./reset";
+
+export const GlobalStyles = createGlobalStyle`
+
+${Reset}
+
+ html, body{
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background-color: #ffffff;
+    color: black;
+    font-size: 1rem;
+    margin: 0;
+ }
+`;
