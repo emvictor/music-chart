61803a89f3b0ff72b81a6454af6eae4a6751ff01
Emival Santos <emivalvictor@hotmail.com>
2021-05-03 17:37:46 -0300

    Fix: vercel deploy

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,4 +2,4 @@ export { default as Header } from "./header";
 export { default as SearchBar } from "./searchbar";
 export { default as MusicCard } from "./music-card";
 export { default as MusicList } from "./music-list";
-export { default as HomeContainer } from "./Home";
+export { default as HomeContainer } from "./home";
